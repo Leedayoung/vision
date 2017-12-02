c288405229850797dcd797541a78cd6b1b5d735c
dayoung <dayoung@postech.ac.kr>
2017-12-03 02:15:04 +0900

    Update dye hair & eye lenz

---
+++ ColorLenz.m
@@ -0,0 +1,44 @@
+function imgFace = ColorLenz(Part,eyeColorSetting,imgFace)
+%LENZ 이 함수의 요약 설명 위치
+%   자세한 설명 위치
+
+for i = 1 : Part(1,4)
+    for j = 1: Part(1,3)
+        img(i,j,1) =imgFace(Part(2)+i,Part(1)+j,1);
+        img(i,j,2) = imgFace(Part(2)+i,Part(1)+j,2);
+        img(i,j,3) = imgFace(Part(2)+i,Part(1)+j,3);
+    end
+end
+imagen = img;
+%  imagen = imgaussfilt(imagen, 2);
+
+%% Convert to gray scale
+if size(imagen,3)==3 % RGB image
+    imagen=rgb2gray(imagen);
+end
+
+%% Convert to binary image
+if (graythresh(imagen)>=0.2)
+    threshold = 0.2;
+else 
+    threshold = graythresh(imagen);
+end
+
+imagen =~im2bw(imagen,threshold);
+%% Remove all object containing fewer than 30 pixels
+imagen = bwareaopen(imagen,5);
+
+I2 = rgb2gray(imgFace);
+for i = 1 : Part(1,4)
+    for j = 1: Part(1,3)
+        if(imagen(i,j) == 1)
+            value = ((255.0 -double(I2(Part(1,2)+i,Part(1,1)+j))*3)/255.0);
+           imgFace(Part(1,2)+i,Part(1,1)+j,1) = eyeColorSetting(1)*value;
+            imgFace(Part(1,2)+i,Part(1,1)+j,2) = eyeColorSetting(2)*value;
+             imgFace(Part(1,2)+i,Part(1,1)+j,3) = eyeColorSetting(3)*value;
+        end 
+    end
+end
+
+
+end
---
+++ findEyeboundary.m
@@ -0,0 +1,37 @@
+function [rePart] = findEyeboundary(Part,imgFace)
+%FINDEYEBOUNDARY 이 함수의 요약 설명 위치
+%   자세한 설명 위치
+A = Part;
+ 
+  x = A(1,1);
+ y = A(1,2);
+ w = A(1,3);
+ h = A(1,4);
+ rePart = Part; 
+ 
+ for i = -6:6
+     h = h+i;    
+     L = imgFace(int32(y+h), int32(x+w), 1)/3+imgFace(int32(y+h), int32(x+w), 2)/3+imgFace(int32(y+h), int32(x+w), 3)/3;
+     R = imgFace(int32(y+h), int32(x), 1)/3+ imgFace(int32(y+h), int32(x), 2)/3+ imgFace(int32(y+h), int32(x), 3)/3;
+     M1 = (imgFace(int32(y+h), int32(x+w/4), 1)/3+ imgFace(int32(y+h), int32(x+w/4), 2)/3+ imgFace(int32(y+h), int32(x+w/4), 3)/3);
+     M2 = (imgFace(int32(y+h), int32(x+w/2), 1)/3+ imgFace(int32(y+h), int32(x+w/2), 2)/3+ imgFace(int32(y+h), int32(x+w/2), 3)/3);
+     M3 = (imgFace(int32(y+h), int32(x+3*w/4), 1)/3+ imgFace(int32(y+h), int32(x+3*w/2), 2)/4+ imgFace(int32(y+h), int32(x+3*w/4), 3)/3);
+     M = M1/3 + M2/3 + M3/3;
+
+     if((L/2+R/2)>=M)
+         diff = (L/2+R/2)-M;
+     else
+         diff = M-(L/2+R/2);
+     end
+
+     lines(i+7) = diff;
+     h = A(1,4);
+ end
+
+index = find(lines==min(min(lines))); 
+index = index-7;
+
+ rePart(4) = rePart(4)+index(1);
+
+
+end
